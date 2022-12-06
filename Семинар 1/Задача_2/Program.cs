c9a1f234d4515f1d72f8994272d9c7981518340a
Rushan <ektoeror777@gmail.com>
2022-12-06 17:56:09 +0300

    задача2

---
+++ Семинар 1/Задача_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// На входе 2 числа и проверяет 1ое число квадрат ли второго
+
+Console.WriteLine("Введите числа");
+string number1 = Console.ReadLine() ?? "";
+string number2 = Console.ReadLine() ?? "";
+
+int number01 = Convert.ToInt32(number1);
+int number02 = Convert.ToInt32(number2);
+
+if (number02 * number02 == number01)
+{
+    Console.WriteLine($"{number01} является квадратом {number02}");
+}
+else
+{
+    Console.WriteLine($"{number01} не является квадратом {number02}");
+}
---
+++ Семинар 1/Задача_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выдавать название дня недели по заданному номеру
+
+Console.WriteLine("Введите число");
+string day = console.ReadLine();
+
+
+
+
+
+
+
+/*switch (switch_on)
+{
+    
+    default:
+}*/
