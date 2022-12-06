9f202d4d72c0a2cc3fcf930b96af6f650403cf83
Rushan <ektoeror777@gmail.com>
2022-12-06 18:12:11 +0300

    задача3/1

--- Семинар 1/Задача_3/Program.cs
+++ Семинар 1/Задача_3/Program.cs
@@ -1,16 +1,37 @@
 ﻿// Выдавать название дня недели по заданному номеру
 
 Console.WriteLine("Введите число");
-string day = console.ReadLine();
+string day = Console.ReadLine()?? "";
 
 
 
 
 
 
-
-/*switch (switch_on)
+switch (day) //
 {
-    
+    case "1":
+        Console.WriteLine("Пн");
+        break;
+    case "2":
+        Console.WriteLine("Вт");
+        break;
+    case "3":
+        Console.WriteLine("Ср");
+        break;
+    case "4":
+        Console.WriteLine("Чт");
+        break;
+    case "5":
+        Console.WriteLine("Пт");
+        break;
+    case "6":
+        Console.WriteLine("Сб");
+        break;
+    case "7":
+        Console.WriteLine("Вс");
+        break;
     default:
-}*/
+        Console.WriteLine("Нет такого дня недели");
+        break;
+}
