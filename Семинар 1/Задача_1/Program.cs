bd582d09d08b1b27e243a09859dfe2307fceece6
Rushan <ektoeror777@gmail.com>
2022-12-06 17:55:31 +0300

    задача1

---
+++ Семинар 1/Задача_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Квадрат числа на вводе
+
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine() ?? ""; ///с консоли всегда Str
+int number = Convert.ToInt32(numberStr);// конвертация в переменной в int 
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");/* $ - оператор для внедрения 
+переменных в строку*/
+
