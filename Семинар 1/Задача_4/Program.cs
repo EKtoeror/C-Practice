62393874177b9f79f34890e7d6acb419d7a04ddc
Rushan <ektoeror777@gmail.com>
2022-12-06 18:37:07 +0300

    задача4

---
+++ Семинар 1/Задача_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// На ввод Число N а на выходе все целые числа от -N до N
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine() ?? "";
+
+int n = Convert.ToInt32(number);
+
+n = Math.Abs(n);// по модулю
+
+for (int i = (-n); i <= n; i++)//цикл
+{
+    Console.Write(i+" ");
+}
