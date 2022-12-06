593777c5669090e183e269f3e4b8ae34d1148733
Rushan <ektoeror777@gmail.com>
2022-12-06 19:01:23 +0300

    задача5

---
+++ Семинар 1/Задача_5/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Принимает на вход 3ех значное число и на выходе показывает последнюю цыфру этого 
+числа*/
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine() ?? "";
+
+int n = Convert.ToInt32(number);
+
+n = Math.Abs(n);// для ввода отрицательных чисел
+
+if ((n>=100) && (n<=999))//для проверки 3ех значности чисел
+    Console.WriteLine(n % 10);// остаток от деления на 10
+else 
+    Console.WriteLine("Введено неверное число");
+
+// так делать нельзя 
+/*
+string number = Console.ReadLine() ?? "";
+Console.WriteLine(number[2]);
+*/
