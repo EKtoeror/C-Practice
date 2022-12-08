ba6a9b05837b9648e5b54ee2f78aaa2698eeef74
Rushan <ektoeror777@gmail.com>
2022-12-08 21:21:54 +0300

    задача2.1

---
+++ Семинар 2/Задача_2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Вывод случайного числа из отрезка [10,99]
+// и показывает наибольшую цифру этого числа
+
+int GetRandomNumber(int left, int right)/*функция возвращающая число 
+из данного отрезка*/
+{
+    Random rnd = new Random();//класс рандом
+    int number = rnd.Next(left, right + 1);
+    return number;
+}
+int GetMaxDigitOfNumber(int number)//функция возвращающая максимальную цыфру числа
+{
+    int firstDigit = number / 10; //определение десятков
+    int secondDigit = number % 10; //определение едениц
+
+    if (firstDigit > secondDigit)
+        return firstDigit;
+    else
+        return secondDigit;
+}
+
+int number = GetRandomNumber(10, 99);
+Console.WriteLine($"Число {number}");
+
+int maxDigit = GetMaxDigitOfNumber(number);
+Console.WriteLine(maxDigit);
