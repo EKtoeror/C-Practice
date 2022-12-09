4fba9d34c73ac068e45346ff3198ba779b1bd4d6
Rushan <ektoeror777@gmail.com>
2022-12-09 20:46:38 +0300

    2.4

---
+++ Семинар 2/Задача_2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿// На входе число и проверяет кратно ли 7 и 23 одновременно
+Console.WriteLine("Введите число: ");
+string n = Console.ReadLine() ?? "";
+
+int number = Convert.ToInt32(n);
+
+number = Math.Abs(number);
+
+void ShowResult(int number)
+{
+    int a7 = number % 7;
+    int a23 = number % 23;
+    if (a7 == 0 && a23 == 0) Console.WriteLine($"Число {number} кратно 7 и 23");
+    else Console.WriteLine($"Число {number} не кратно 7 и 23");
+}
+
+ShowResult(number);
+
