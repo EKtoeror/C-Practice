ac3eb1ed82c23b5be96d71d4ed0458640988f936
Rushan <ektoeror777@gmail.com>
2022-12-11 20:18:53 +0300

    3.1

---
+++ Семинар 3/Задача_3.1/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Напишите программу которая принимает координаты точки (x и y)
+причем они не равны 0 и выдает номер четверти плоскости у которой она находится*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число");
+        }
+    }
+    return result;
+}
+
+int GetQuarter(int x, int y)
+{
+    if (x > 0 && y > 0)
+        return 1;
+    if (x > 0 && y < 0)
+        return 4;
+    if (x < 0 && y < 0)
+        return 3;
+    return 2;
+}
+
+int x = GetNumber("Введите координату x: ");
+int y = GetNumber("Введите координату y: ");
+int quarter = GetQuarter(x, y);
+
+Console.WriteLine($"Точка с координатами {x}, {y} лежит в {quarter} четверти");
