1f7f015fbc681364eb4b79b9a71eafccd911dd3b
Rushan <ektoeror777@gmail.com>
2022-12-11 23:02:58 +0300

    3.4

---
+++ Семинар 3/Задача_3.4/Program.cs
@@ -0,0 +1,10 @@
+﻿// на входе число Н выдает таблицу квадратов числе от 1 до Н
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i =1 ; i<= number ; i++)
+{
+    int result = i*i;
+    Console.WriteLine($" {i}^2 = {result}");
+}
